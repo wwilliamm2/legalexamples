496d5e4abf139e7f2394b1c1cdf78217c1494b71
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-05-31 21:39:01 -0700

    more

--- public/rabata_demo.bash
+++ public/rabata_demo.bash
@@ -7,26 +7,30 @@ echo $RABATAKEY10
 echo $RABATABUCKET
 
 echo s3.rcs.rabata.io
+echo s3.amazonaws.com
 
-exit
-
-file=/path/to/file/to/upload.tar.gz
-bucket=your-bucket
+file=upload.tar.gz
+bucket=$RABATABUCKET
 resource="/${bucket}/${file}"
 contentType="application/x-compressed-tar"
 dateValue=`date -R`
 stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-s3Key=xxxxxxxxxxxxxxxxxxxx
-s3Secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+s3Key=$RABATAKEY10
+s3Secret=$RABATASK
 signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
-curl -X PUT -T "${file}" \
-  -H "Host: ${bucket}.s3.amazonaws.com" \
+
+/home/lc11/anaconda3/envs/lx12/bin/curl -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.rcs.rabata.io" \
   -H "Date: ${dateValue}" \
   -H "Content-Type: ${contentType}" \
   -H "Authorization: AWS ${s3Key}:${signature}" \
-  https://${bucket}.s3.amazonaws.com/${file}
+  https://${bucket}.s3.rcs.rabata.io/${file}
+
+exit
 
 # https://glacius.tmont.com/articles/uploading-to-s3-in-bash
 # https://gist.github.com/chrismdp/6c6b6c825b07f680e710
 # https://stackoverflow.com/questions/44751574/uploading-to-amazon-s3-via-curl-route
+# s3.amazonaws.com == s3.rcs.rabata.io
 
+# FAILS
