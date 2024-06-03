3140c65f4033f206ba9799f05538ce48ac142bf9
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-03 16:57:06 -0700

    more

---
+++ public/rails_g_counties.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ~/lx/lx14/public/rails_g_counties.bash
+
+rails g controller Counties \
+      contra_costa fresno los_angeles orange_county riverside sacramento san_bernadino \
+      san_luis_obispo san_mateo santa_clara santa_cruz sonoma
+
+exit
+
