37be79ea7d96e05e0ad6e45df8c6891eea67cfe9
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-04 10:04:39 -0700

    more

--- public/get_lacourt_file_list.bash
+++ public/get_lacourt_file_list.bash
@@ -6,7 +6,8 @@
 
 # /bin/ls -1 ~tent1/tent1/lacourt/htmls/2*html > lacourt_file_list.txt
 
-/bin/ls -1 ~/lx/jffj/lacourt/htmls/2*html > lacourt_file_list.txt
+/bin/ls -1 ~/lx/jffj/lacourt/htmls/2*html > tmp.txt
+cat tmp.txt | sed '1,$s:/home/lc11/lx/jffj/lacourt/htmls/::' > lacourt_file_list.txt
 
 # These files shd also be available from jffjorg github repo.
 
