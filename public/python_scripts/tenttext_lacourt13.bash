8259ab6d761e4520128e46cd8f8ea6446b73313a
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-05 15:21:11 -0700

    more

--- public/python_scripts/tenttext_lacourt13.bash
+++ public/python_scripts/tenttext_lacourt13.bash
@@ -5,10 +5,9 @@
 # Helps me prep text files for github.io repo for serving
 
 rsync -av /tmp/lacourt_text/202* ~/lx/jffj/lacourt/text/
-du -sh
+du -sh ~/lx/jffj/lacourt/text/
 
 # cd ~/lx/jffj/lacourt/
 # git commit -am more-text
 # git push origin main
 exit
-
