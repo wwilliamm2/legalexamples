036aa2ef0cf2567eb4911aa61445db58a4cbf115
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-06 10:39:42 -0700

    more

---
+++ public/python_scripts/gen_summaries10.py
@@ -0,0 +1,12 @@
+'''~/lx/lx14/public/py/gen_summaries10.py'''
+
+'''
+Reads filenames from tenttext_lacourt14.txt
+to generate llm-summaries of the corresponding text files.
+
+Ref:
+~dan/lc17/
+'''
+
+'done'
+
