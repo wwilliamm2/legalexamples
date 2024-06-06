cbfc5e52b9155cf54e19c877a01c82f130984b55
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-05 19:22:09 -0700

    more

--- public/python_scripts/tenttext_lacourt13.bash
+++ public/python_scripts/tenttext_lacourt13.bash
@@ -11,15 +11,22 @@ du -sh ~/lx/jffj/lacourt/text/
 # git add text
 # git commit -am more-text
 # git push origin main
+
+# /bin/ls -1 ~/lx/jffj/lacourt/text/202*/tent*.txt > ~/lx/lx14/public/py/tenttext_lacourt14.txt
+# bash: /bin/ls: Argument list too long
+# darn
+# Try listing fewer files:
 /bin/ls -1 ~/lx/jffj/lacourt/text/2023_0*/tent*.txt > ~/lx/lx14/public/py/tenttext_lacourt14.txt
 /bin/ls -1 ~/lx/jffj/lacourt/text/2023_1*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
 /bin/ls -1 ~/lx/jffj/lacourt/text/2024_0*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
 /bin/ls -1 ~/lx/jffj/lacourt/text/2024_1*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
+/bin/ls -1 ~/lx/jffj/lacourt/text/2025_0*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
+/bin/ls -1 ~/lx/jffj/lacourt/text/2025_1*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
 
 cd ~/lx/lx14/public/py/
 git add .
 git commit -am more-text
-git push origin main
+git push ghw2 main
 bash ../deploylx14.bash
 
 exit
