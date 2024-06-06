1d07862ea3029f97245313ec90ae915e3d0ed8a1
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-06 10:49:26 -0700

    more

--- public/python_scripts/gen_summaries10.py
+++ public/python_scripts/gen_summaries10.py
@@ -5,8 +5,14 @@ Reads filenames from tenttext_lacourt14.txt
 to generate llm-summaries of the corresponding text files.
 
 Ref:
-~dan/lc17/
+~dan/lc17/rt20.py
 '''
+
+'Summarize this large file:'
+
+txt_f_s = 'lacourt/text/2024_07_19_LAM_71/tent_ruling_01.txt'
+
+
 
 'done'
 
