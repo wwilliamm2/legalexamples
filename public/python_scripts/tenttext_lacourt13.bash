90613790feb172cd34a6214cc75f6d71678ac6ac
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-05 18:39:03 -0700

    more-text

--- public/python_scripts/tenttext_lacourt13.bash
+++ public/python_scripts/tenttext_lacourt13.bash
@@ -8,6 +8,18 @@ rsync -av /tmp/lacourt_text/202* ~/lx/jffj/lacourt/text/
 du -sh ~/lx/jffj/lacourt/text/
 
 # cd ~/lx/jffj/lacourt/
+# git add text
 # git commit -am more-text
 # git push origin main
+/bin/ls -1 ~/lx/jffj/lacourt/text/2023_0*/tent*.txt > ~/lx/lx14/public/py/tenttext_lacourt14.txt
+/bin/ls -1 ~/lx/jffj/lacourt/text/2023_1*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
+/bin/ls -1 ~/lx/jffj/lacourt/text/2024_0*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
+/bin/ls -1 ~/lx/jffj/lacourt/text/2024_1*/tent*.txt >> ~/lx/lx14/public/py/tenttext_lacourt14.txt
+
+cd ~/lx/lx14/public/py/
+git add .
+git commit -am more-text
+git push origin main
+bash ../deploylx14.bash
+
 exit
