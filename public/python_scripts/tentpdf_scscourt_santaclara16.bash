9fe82568abd17b9a2f278464b40461c99b995905
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-16 12:22:58 -0700

    more-pdf

--- public/python_scripts/tentpdf_scscourt_santaclara16.bash
+++ public/python_scripts/tentpdf_scscourt_santaclara16.bash
@@ -11,4 +11,9 @@
 # /bin/ls -1 ~/lx/jffj/scscourt/pdfs/2024_0*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
 # /bin/ls -1 ~/lx/jffj/scscourt/pdfs/2024_1*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
 
+
+git add .
+git commit -am more-pdf
+git push ghw2 main
+
 exit
