76207c7eebf09df32584a67a1d118252dfb3cab0
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-16 12:21:48 -0700

    more

--- public/python_scripts/tentpdf_scscourt_santaclara15.bash
+++ public/python_scripts/tentpdf_scscourt_santaclara15.bash
@@ -3,6 +3,13 @@
 
 # Copies pdf files to the right place.
 rsync -av ~tent1/tent1/scscourt/pdfs/2023_0*pdf ~/lx/jffj/scscourt/pdfs/
+rsync -av ~tent1/tent1/scscourt/pdfs/2023_1*pdf ~/lx/jffj/scscourt/pdfs/
+rsync -av ~tent1/tent1/scscourt/pdfs/2024*pdf ~/lx/jffj/scscourt/pdfs/
+
+cd ~/lx/jffj/
+git add .
+git commit -am more-pdf
+git push origin main
 
 exit
 
---
+++ public/python_scripts/tentpdf_scscourt_santaclara16.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# tentpdf_scscourt_santaclara16.bash
+
+/bin/ls -1 ~/lx/jffj/scscourt/pdfs/2023_0*pdf > ~/lx/lx14/public/py/tentpdf_scscourt17.txt
+/bin/ls -1 ~/lx/jffj/scscourt/pdfs/2023_1*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
+
+/bin/ls -1 ~/lx/jffj/scscourt/pdfs/2024_0*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
+# /bin/ls -1 ~/lx/jffj/scscourt/pdfs/2024_1*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
+
+# /bin/ls -1 ~/lx/jffj/scscourt/pdfs/2024_0*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
+# /bin/ls -1 ~/lx/jffj/scscourt/pdfs/2024_1*pdf >> ~/lx/lx14/public/py/tentpdf_scscourt17.txt
+
+exit
