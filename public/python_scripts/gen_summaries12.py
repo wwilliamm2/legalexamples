9310819e081bb08020802310726e7ab6db10199f
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-06-24 14:10:23 -0700

    more

--- public/python_scripts/gen_summaries12.py
+++ public/python_scripts/gen_summaries12.py
@@ -64,7 +64,7 @@ tr_fn_s_l = sorted(tr_files_s.split('\n')[:-1]) # skip last 1
 
 # for tr_fn_s in tr_fn_s_l:
 # debug
-for tr_fn_s in tr_fn_s_l[-18199:]:
+for tr_fn_s in tr_fn_s_l[-19199:]:
     # debug
     summ_fn_s = tr_fn_s.replace('/tent_ruling_','/summary_')
     'I shd check if summ_fn_s exists so I dont overwrite it'
