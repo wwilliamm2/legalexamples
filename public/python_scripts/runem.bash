10b9a1bf1365774affa740bbb5e0bda7d1a6e5a8
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-07-09 18:30:42 -0700

    more

---
+++ public/python_scripts/runem.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# ~/lx/legalexamples/public/python_scripts/runem.bash
+# Runs some scripts I like to run.
+
+cd ~/lx/legalexamples/public/python_scripts/
+
+/bin/bash tenttext_lacourt10.bash
+. ~/lx/lx12_env.bash
+~/anaconda3/envs/lx12/bin/python tenttext_lacourt11.py
+exit
+/bin/bash tenttext_lacourt12.bash
+exit
+/bin/bash tenttext_lacourt13.bash
+/bin/bash tenttext_lacourt15.bash
