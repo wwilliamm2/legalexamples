cc3c6af9f1395a1f2e8251d98624c330e3cdd1e6
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-07-09 19:06:28 -0700

    more-text

--- public/python_scripts/runem.bash
+++ public/python_scripts/runem.bash
@@ -8,8 +8,9 @@ cd ~/lx/legalexamples/public/python_scripts/
 /bin/bash tenttext_lacourt10.bash
 . ~/lx/lx12_env.bash
 ~/anaconda3/envs/lx12/bin/python tenttext_lacourt11.py
-exit
 /bin/bash tenttext_lacourt12.bash
-exit
+
 /bin/bash tenttext_lacourt13.bash
+
+exit
 /bin/bash tenttext_lacourt15.bash
