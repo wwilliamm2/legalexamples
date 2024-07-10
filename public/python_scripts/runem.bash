e1e075303d5a4fc1b9bca498e1e4d6ed6aa1e7e9
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-07-09 21:51:02 -0700

    more

--- public/python_scripts/runem.bash
+++ public/python_scripts/runem.bash
@@ -4,13 +4,25 @@
 # Runs some scripts I like to run.
 
 cd ~/lx/legalexamples/public/python_scripts/
+date
 
 /bin/bash tenttext_lacourt10.bash
+date
+
 . ~/lx/lx12_env.bash
+
 ~/anaconda3/envs/lx12/bin/python tenttext_lacourt11.py
+date
+
 /bin/bash tenttext_lacourt12.bash
+date
 
 /bin/bash tenttext_lacourt13.bash
+date
+
+/bin/bash tenttext_lacourt15.bash
+date
+
+# ~/anaconda3/envs/lx12/bin/python gen_summaries12.py
 
 exit
-/bin/bash tenttext_lacourt15.bash
