e7c70fd3db41166befb60df8b1d9bb5a312a8363
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-07-21 18:25:46 -0700

    more

--- public/get_lacourt_file_list.bash
+++ public/get_lacourt_file_list.bash
@@ -6,6 +6,9 @@
 
 # /bin/ls -1 ~tent1/tent1/lacourt/htmls/2*html > lacourt_file_list.txt
 
+# copy recent html files to the right place:
+rsync -a ~tent1/tent1/lacourt/htmls/2*html ~/lx/jffj/lacourt/htmls/
+
 /bin/ls -1 ~/lx/jffj/lacourt/htmls/2*html > tmp.txt
 cat tmp.txt | sed '1,$s:/home/lc11/lx/jffj/lacourt/htmls/::' > lacourt_file_list.txt
 
