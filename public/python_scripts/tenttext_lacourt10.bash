e0df156729308e608ccd536527ebbaa17b52699f
bob smith maybe <bobsmithmaybe@qoutlook.com>
2024-07-21 18:31:33 -0700

    more

--- public/python_scripts/tenttext_lacourt10.bash
+++ public/python_scripts/tenttext_lacourt10.bash
@@ -8,4 +8,10 @@ rm -rf /tmp/lacourt_text/
 # done
 # rsync -av ~tent1/tent1/lacourt/htmls/2023* /tmp/lacourt_htmls/
 rsync -av ~tent1/tent1/lacourt/htmls/2024*   /tmp/lacourt_htmls/
+
+
+# do this too [to create lacourt_file_list.txt]
+cd ~/lx/lx14/public/
+bash ~/lx/lx14/public/get_lacourt_file_list.bash
+
 exit
