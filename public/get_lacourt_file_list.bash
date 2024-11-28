7027b8e4fac8772c598aa8177748816afbe13ea1
Fred Tealyodeler <fred@fredt.edu>
2024-11-27 21:16:35 -0800

    more

--- public/get_lacourt_file_list.bash
+++ public/get_lacourt_file_list.bash
@@ -10,7 +10,7 @@ cd ~dan/lx/lx14/public/
 # rsync -a ~dan/lx/tent1/lacourt/htmls/2*html ~dan/lx/jffj/lacourt/htmls/
 rsync -a tent1@d81:tent1/lacourt/htmls/2*html ~dan/lx/jffj/lacourt/htmls/
 
-/bin/ls -1 ~dan/lx/jffj/lacourt/htmls/2*html > tmp.txt
+/bin/ls -1 /home/dan/lx/jffj/lacourt/htmls/2*html > tmp.txt
 cat tmp.txt | sed '1,$s:/home/dan/lx/jffj/lacourt/htmls/::' > lacourt_file_list.txt
 
 # These files shd also be available from jffjorg github repo.
