c4d9aa6a8e91d1d1ee22114405bf9e9efbc91ef3
Fred Tealyodeler <fred@fredt.edu>
2025-02-03 17:35:37 -0800

    more

---
+++ public/python_scripts/gen_summaries14.py
@@ -0,0 +1,90 @@
+'''~/lx/lx14/public/py/gen_summaries14.py'''
+
+'''
+Reads filenames from tenttext_lacourt14.txt
+to generate llm-summaries of the corresponding text files.
+
+Ref:
+gen_summaries10.py
+gen_summaries11.py
+~dan/lc17/rt20.py
+
+Demo:
+ll ~/anaconda3/envs/
+. ~/lx/lx14/public/lx14b_env.bash
+python gen_summaries14.py
+
+'''
+
+print('Busy ...')
+import bs4, datetime, glob, inspect, json, operator, os, re, shutil, sys, time, typing
+
+import langchain, langchain_core, langsmith, langchain_groq
+
+myts = str(datetime.datetime.now()).replace(' ','_')
+
+os.environ["GROQ_API_KEY"]
+
+LLM_NAME = 'llama3-8b-8192'
+
+myllm_model = langchain_groq.ChatGroq(model=LLM_NAME)
+
+parser = langchain_core.output_parsers.StrOutputParser()
+
+# create prompt list of tpls acting as messages
+prompt_s = '''Please write a simple summary in YAML format of a legal document, a
+"tentative ruling", written by a Judge about a future Court Hearing. 
+The legal document describes the Judge's thinking based on infomation presented 
+to the Judge before today.
+In YAML format, please write: Case Number, Plaintiff's Name, Defendant's Name,
+People-Names, Company-Names, Places, Dates, Events, Money, 
+Intentions, Facts, Disputes, Acts, Agreements, Laws, Case Citations, 
+Statutes, Roles and Signifcant Statements.
+I really appreciate your help!
+Oh, also please write English-text to summarize this judge's tentative ruling: '''
+
+user_tpl = ("user", prompt_s + "\n[[{tent_ruling}]]")
+
+messages_l = [user_tpl]
+
+prompt = langchain_core.prompts.ChatPromptTemplate.from_messages(messages_l)
+
+'I use this to connect prompt to groq myllm_model:'
+chain = prompt | myllm_model | parser
+
+'''Use tenttext_lacourt14.txt, 
+a file full of tr-file-names to drive a loop 
+which calls LLM to summarize each tr file.'''
+
+with open('tenttext_lacourt14.txt') as txtf:
+    tr_files_s = txtf.read()
+tr_fn_s_l = sorted(tr_files_s.split('\n')[:-1]) # skip last 1
+
+for tr_fn_s in tr_fn_s_l[-3:]:
+    summ_fn_s = tr_fn_s.replace('/tent_ruling_','/summary_')
+    with open(tr_fn_s) as txtf:
+        tr_pln_txt_s = txtf.read()
+    if len(tr_pln_txt_s) > 700: # It interests me
+        'Note the file name:'
+        print(f'Summarizing this file: {tr_fn_s}')
+        print(f'Its length in chars: {len(tr_pln_txt_s)}')
+        print('Busy with API ...')
+        'I shd call LLM here.'
+        context_i = 99011 # large-ish
+        'Prep a dict to help me call invoke():'
+        invoke_d = {'tent_ruling': tr_pln_txt_s[:context_i]}
+        'Rubber meets road:'
+        try:
+            summary_s = chain.invoke(invoke_d)
+            with open(f'{summ_fn_s}', 'a') as sumf:
+                top_s = f'..................... ..................... ..................... ......\nThe summary displayed below was created by an LLM named "{LLM_NAME}":\n'
+                sumf.write(f'{top_s}{summary_s}\n')
+                print(f'New summary: {summ_fn_s}')
+            # Make note of invoke_d
+            with open(f'/tmp/invoke_d.txt', 'w') as invdf:
+                invdf.write(str(prompt.invoke(invoke_d)))
+            'remem to throttle API calls:'
+            print('Busy with API ...')
+            time.sleep(6) # seconds
+        except:
+            time.sleep(400) # 10min
---
+++ public/python_scripts/tmp.py
@@ -0,0 +1,2 @@
+with open(f'/tmp/tmp1.txt', 'w') as f:
+    f.write('hello.\n')
