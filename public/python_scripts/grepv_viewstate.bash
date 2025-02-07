331f96c983df7eba5f7cfc52b8dbdb9194654278
Fred Tealyodeler <fred@fredt.edu>
2025-02-07 03:12:14 -0800

    more

--- public/python_scripts/grepv_viewstate.bash
+++ public/python_scripts/grepv_viewstate.bash
@@ -4,6 +4,12 @@
 
 # Filters out some bad input elements.
 
-rsync -a ~/lx/jffj/lacourt/htmls /tmp/lacourt/
+# rsync -a ~/lx/jffj/lacourt/htmls /tmp/lacourt/
+cd /tmp/lacourt/htmls/
+for htmlf in 2025_*html
+do
+    grep -v __VIEWSTATE $htmlf > good.html
+    mv good.html $htmlf
+done
 
 
