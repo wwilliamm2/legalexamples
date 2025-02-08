a7d605240f12b34a34e6d8df9b0c8062728d225f
Fred Tealyodeler <fred@fredt.edu>
2025-02-07 18:49:06 -0800

    more

--- public/python_scripts/clean_html4LLM15.py
+++ public/python_scripts/clean_html4LLM15.py
@@ -17,6 +17,9 @@ import bs4, datetime, glob, inspect, operator, os, re, shutil, sys, time
 from bs4 import BeautifulSoup, Comment
 
 fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/htmls/202*.html'))[:33]
+# Define the regular expression pattern
+div_re_pattern = r'<div>\s*</div>'
+span_re_pattern = r'<span>\s*</span>'
 
 for fn_s in fn_s_l:
     with open(fn_s, 'r', encoding='utf-8') as cbf:
@@ -35,7 +38,18 @@ for fn_s in fn_s_l:
     fn_enh_s = fn_s.replace('/htmls/','/enhanced_htmls/')    
     # Write the modified HTML to a new file
     with open(fn_enh_s, 'w', encoding='utf-8') as enf:
-        enh_html_s = str(soup).replace('<div></div>','').replace('<span></span>','')
+        # Replace re pattern matches with an empty string
+        nodiv_html_s = re.sub(div_re_pattern, '', str(soup))
+        enh_html_s = re.sub(span_re_pattern, '', nodiv_html_s)
         enf.write(enh_html_s)
 'done'
 
+'''
+gpt, Please write a simple Python demo which uses the re package to replace
+'<div>   </div>' with ''
+using a regular expression to match 0 or more space characters between the two div-tags.
+
+Answer:
+div_re_pattern = r'<div>\s*</div>'
+clean_s = re.sub(div_re_pattern, '', dirty_s)
+'''
