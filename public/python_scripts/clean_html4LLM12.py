70b9cc6121e0e664387dfe787300d2cf159b1bab
Fred Tealyodeler <fred@fredt.edu>
2025-02-07 16:02:06 -0800

    more

---
+++ public/python_scripts/clean_html4LLM12.py
@@ -0,0 +1,24 @@
+'''~/lx/lx14/public/py/clean_html4LLM12.py'''
+
+'''
+Cleans my.html so my LLM can more easily parse my.html.
+Removes html-elements: anchor , script , style , link , comments , form , input
+Removes attributes: id , class
+
+Demo:
+. ~/lx/lx14/public/lx14b_env.bash
+[ or conda activate lx14b ]
+python -i clean_html4LLM12.py
+'''
+
+print('Busy ...')
+import bs4, datetime, glob, inspect, operator, os, re, shutil, sys, time
+
+from bs4 import BeautifulSoup
+
+with open('my.html', 'r', encoding='utf-8') as cbf:
+    soup = BeautifulSoup(cbf, 'lxml')
+'I want to see the text in my.html'    
+large_s = soup.text
+with open('soup.txt', 'w') as tf:
+    tf.write(large_s)
