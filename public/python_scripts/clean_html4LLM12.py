eb573da8070c775565f3a4285f5024bccf8559d7
Fred Tealyodeler <fred@fredt.edu>
2025-02-07 16:13:08 -0800

    more

--- public/python_scripts/clean_html4LLM12.py
+++ public/python_scripts/clean_html4LLM12.py
@@ -17,8 +17,17 @@ import bs4, datetime, glob, inspect, operator, os, re, shutil, sys, time
 from bs4 import BeautifulSoup
 
 with open('my.html', 'r', encoding='utf-8') as cbf:
-    soup = BeautifulSoup(cbf, 'lxml')
+    soup = BeautifulSoup(cbf, 'html.parser')
 'I want to see the text in my.html'    
 large_s = soup.text
 with open('soup.txt', 'w') as tf:
     tf.write(large_s)
+
+# Find and remove all unneeded elements
+tag_l = ['a', 'script', 'style', 'link', 'form', 'input']
+for elmnt in soup(tag_l):
+    elmnt.decompose() # They should have called it delete() or rm() or ...
+
+# Write the modified HTML to a new file
+with open('my_enhanced.html', 'w', encoding='utf-8') as enf:
+    enf.write(str(soup))
