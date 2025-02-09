f83524f36efd648fe35afe1bcd837b8f996cd191
Fred Tealyodeler <fred@fredt.edu>
2025-02-09 13:15:45 -0800

    more

---
+++ public/python_scripts/split_pdf_demo2.py
@@ -0,0 +1,35 @@
+'''~/lx/lx14/public/py/split_pdf_demo2.py'''
+
+'''
+Splits pdfs into folders of pages. I get both pdf pages and png pages.
+
+Demo:
+
+conda activate gemini1
+python split_pdf_demo2.py
+
+'''
+
+# Use bash to make a folder for each pdf:
+# cd ~/ssd2/tmp/scscourt/pdfs/
+# ls -1 202*.pdf | sed 's/^/mkdir /' | sed 's/.pdf$//' > /tmp/mkdirem.bash
+# bash /tmp/mkdirem.bash
+
+'gpt, Please write a Python demo to get file name from a path'
+
+import PyPDF2, glob, os
+
+fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf'))[:3]
+
+for fn_s in fn_s_l:
+    fn_ospbn_s = os.path.basename(fn_s).replace('.pdf','')
+    with open(fn_s, 'rb') as file:
+        pdf_reader = PyPDF2.PdfReader(file)
+        for i, page in enumerate(pdf_reader.pages):
+            pdf_writer = PyPDF2.PdfWriter()
+            pdf_writer.add_page(page)
+            folder_n_s = fn_s.replace('.pdf','/')
+            pg_n_s = f"{folder_n_s}{fn_ospbn_s}_page{i+1:02d}.pdf"
+            print(pg_n_s)
+            with open(pg_n_s, 'wb') as page_f:
+                'pdf_writer.write(page_f)'
