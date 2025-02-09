2be3cb06f89d0b7765bb0fc9a0862a8124cc7e25
Fred Tealyodeler <fred@fredt.edu>
2025-02-09 13:47:40 -0800

    more

--- public/python_scripts/split_pdf_demo2.py
+++ public/python_scripts/split_pdf_demo2.py
@@ -19,20 +19,24 @@ python split_pdf_demo2.py
 
 import PyPDF2, glob, os
 
-fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf'))[:3]
+fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf'))[:33]
 magick_s = '/home/dan/anaconda3/envs/imagemagick/bin/magick'
 
 for fn_s in fn_s_l:
     fn_ospbn_s = os.path.basename(fn_s).replace('.pdf','')
     with open(fn_s, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
-        for i, page in enumerate(pdf_reader.pages):
-            pdf_writer = PyPDF2.PdfWriter()
-            pdf_writer.add_page(page)
-            folder_n_s = fn_s.replace('.pdf','/')
-            pg_n_s = f"{folder_n_s}{fn_ospbn_s}_page{i+1:02d}.pdf"
-            png_n_s = pg_n_s.replace('.pdf', '.png')
-            with open(pg_n_s, 'wb') as page_f:
-                pdf_writer.write(page_f)
-            os.system(f'{magick_s} {pg_n_s} {png_n_s}')
-            print(png_n_s)
+        try:
+            pdf_reader = PyPDF2.PdfReader(file)
+            for i, page in enumerate(pdf_reader.pages):
+                pdf_writer = PyPDF2.PdfWriter()
+                pdf_writer.add_page(page)
+                folder_n_s = fn_s.replace('.pdf','/')
+                pg_n_s = f"{folder_n_s}{fn_ospbn_s}_page{i+1:02d}.pdf"
+                png_n_s = pg_n_s.replace('.pdf', '.png')
+                with open(pg_n_s, 'wb') as page_f:
+                    pdf_writer.write(page_f)
+                    os.system(f'{magick_s} {pg_n_s} {png_n_s}')
+                    print(png_n_s)
+        except Exception as myex:
+            print(f'I see a problematicfile: {fn_s}')
+            print(f'Due to: {str(myex)}')
