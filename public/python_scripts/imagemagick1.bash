ca55cb42bbba7cfd5006e44916cd6f7b4880d9ed
Fred Tealyodeler <fred@fredt.edu>
2025-02-09 16:06:36 -0800

    more

---
+++ public/python_scripts/imagemagick1.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# ~/lx/lx14/public/py/imagemagick1.bash
+
+# Uses a loop to call magick on some pdf files
+
+# Demo:
+# conda activate imagemagick
+# bash ~/lx/lx14/public/py/imagemagick1.bash
+
+echo started:
+date
+
+fldr=/media/dan/ssd2/tmp/scscourt/pdfs/
+for pdf in ${fldr}2023_0916*tues*/2*page*.pdf
+do
+    echo ${pdf}.png
+    if [ -f ${pdf}.png ]; then
+	echo magick did this already.
+    else
+	magick $pdf ${pdf}.png
+    fi
+done
+
+echo finished:
+date
+
+exit
+
+# gpt, please write demo Bash syntax which returns true if /tmp/abc.tx exists
+
+if [ -f "/tmp/abc.tx" ]; then
+    echo "File exists"
+    exit 0
+else
+    echo "File does not exist"
+    exit 1
+fi
--- public/python_scripts/split_pdf_demo2.py
+++ public/python_scripts/split_pdf_demo2.py
@@ -1,7 +1,8 @@
 '''~/lx/lx14/public/py/split_pdf_demo2.py'''
 
 '''
-Splits pdfs into folders of pages. I get both pdf pages and png pages.
+Splits pdfs into folders of pages.
+I rely on a bash script to convert each page pdf to a page png.
 
 Demo:
 
@@ -19,11 +20,12 @@ python split_pdf_demo2.py
 
 import PyPDF2, glob, os, time
 
-fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf'))[:3]
+fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf'))
 magick_s = '/home/dan/anaconda3/envs/imagemagick/bin/magick'
 
 for fn0_s in fn_s_l:
     try:
+        print(fn0_s)
         fn_s = fn0_s.replace(' ','_')
         fn_ospbn_s = os.path.basename(fn_s).replace('.pdf','')
         with open(fn_s, 'rb') as file:
@@ -36,9 +38,9 @@ for fn0_s in fn_s_l:
                 png_n_s = pg_n_s.replace('.pdf', '.png')
                 with open(pg_n_s, 'wb') as page_f:
                     pdf_writer.write(page_f)
-                    time.sleep(1) # throttle
-                    os.system(f'{magick_s} {pg_n_s} {png_n_s}')
-                    print(png_n_s)
     except Exception as myex:
         print(f'I see a problematic file: {fn0_s}')
         print(f'Due to: {str(myex)}')
+# Now I can call:
+# conda activate imagemagick
+# bash ~/lx/lx14/public/py/imagemagick1.bash
