508cc171c5996df3367a91d135aaa07fca30ce48
Fred Tealyodeler <fred@fredt.edu>
2025-02-10 15:58:30 -0800

    more

--- public/python_scripts/gemini_flash_text_from_png16.py
+++ public/python_scripts/gemini_flash_text_from_png16.py
@@ -8,29 +8,17 @@ conda activate gemini1
 python -i gemini_flash_text_from_png16.py
 '''
 
-from google import genai
-
-# Check Google AI Studio API key
-import os
-
 print('Busy ...')
-import datetime, glob, inspect, json, operator, re, shutil, sys, time, typing
+
+import datetime, glob, inspect, json, operator, os, re, shutil, sys, time, typing
+
+from google import genai
 
 llm_s_l = ['gemini-2.0-flash-lite-preview','gemini-2.0-flash-exp']
 
 model_id = llm_s_l[0]
 
-fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/2023_0916_2245_33_dept10_tues/2*png'))
-
-fn_s = fn_s_l[0]
-
 client = genai.Client(api_key=os.environ["GOOGLE_API_KEY"])
-
-doc_png = client.files.upload(file=fn_s, config={'display_name': fn_s})
-
-file_size = client.models.count_tokens(model=model_id,contents=doc_png)
-
-print(f'File: {doc_png.display_name} equals to {file_size.total_tokens} tokens')
 
 prompt_s = '''Please study information from my file and then extract (verbatim) all text you see in it.
 My file is an image of a single page in California legal document filed in a county court.
@@ -42,12 +30,21 @@ I want you to preserve the format because I want your extracted and verbatim tex
 Thanks!
 '''
 
-response = client.models.generate_content(model=model_id, contents=[doc_png, prompt_s])
+fn_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/2023_0916_2245_33_dept10_tues/2*png'))[:4]
 
-try:
-    # Extract and print only the "text" field from the response
-    if response.candidates:
-        response.candidates[0].content.parts[0].text
-        print(response.candidates[0].content.parts[0].text)
-except Exception as myexp:
-    print('I see a problem with {fn_s}\ndue to: ', str(myexp))
+# fn_s = fn_s_l[0]
+
+for fn_s in fn_s_l:
+    try:
+        fn_txt_s = f'{fn_s}.txt'
+        print('Working on: ', fn_txt_s)
+        doc_png = client.files.upload(file=fn_s, config={'display_name': fn_s})
+        file_size = client.models.count_tokens(model=model_id,contents=doc_png)
+        print(f'File: {doc_png.display_name} equals to {file_size.total_tokens} tokens')
+        response = client.models.generate_content(model=model_id, contents=[doc_png, prompt_s])
+        # Extract and print only the "text" field from the response
+        if response.candidates:
+            with open(fn_txt_s, 'w') as txtf:
+                txtf.write(response.candidates[0].content.parts[0].text)
+    except Exception as myexp:
+        print('I see a problem with {fn_s}\ndue to: ', str(myexp))
