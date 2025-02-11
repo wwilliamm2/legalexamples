10c034e191c625529f145271dfdd0be734f78a44
Fred Tealyodeler <fred@fredt.edu>
2025-02-11 00:59:08 -0800

    more

--- public/python_scripts/filecmp1.py
+++ public/python_scripts/filecmp1.py
@@ -1,7 +1,7 @@
 '''~/lx/lx14/public/py/filecmp1.py'''
 
 '''
-Creates a dictionary of keys which point to twins of the keys.
+Creates a list of duplicate files to rm.
 
 Demo:
 Python -i filecmp1.py
@@ -9,18 +9,18 @@ Python -i filecmp1.py
 
 import glob, filecmp
 
-fn1_s_l = glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf')
+fn1_s_l = sorted(glob.glob('/media/dan/ssd2/tmp/scscourt/pdfs/202*pdf'))
 
-fn2_s_l = list(fn1_s_l)
+fn2_s_l = sorted(list(fn1_s_l))
 
 matches_l = []
 for fn1_s in fn1_s_l:
     for fn2_s in fn2_s_l:
-        if filecmp.cmp(fn1_s, fn2_s, shallow=False):
-            # Filter out previous matches            
-            if fn1_s < fn2_s:
+        # Filter out redundant matches
+        if fn1_s < fn2_s:
+            if filecmp.cmp(fn1_s, fn2_s, shallow=False):
                 matches_l.append([fn1_s, fn2_s])
-matches_l
-for fn_s_l in matches_l:
-    # rm the oldest file
-    print('rm ',fn_s_l[0])
+dups_s_l = [fn_s_l[0] for fn_s_l in matches_l]
+rm_these_l = sorted(list(set(dups_s_l)))
+for fn_s in rm_these_l:
+    print('rm ', fn_s)
