cdf3767201557c88415fcb8b2ec5d39d29be066e
Fred Tealyodeler <fred@fredt.edu>
2025-03-01 12:57:22 -0800

    more

--- public/python_scripts/pdf_magick_tess10.bash
+++ public/python_scripts/pdf_magick_tess10.bash
@@ -10,13 +10,20 @@
 
 pdfn='/home/dan/gsc1/cases/case_types/real_property/34-2021-00313325-CU-OR-GDS/34-2021-00313325-CU-OR-GDS_03_12_30_2021_Complaint_McElhaney_Roddie_Pl.pdf'
 
-pngn=`echo $pdfn | sed 's/.pdf$/.png/'`
+pngn=`echo $pdfn | sed 's/.pdf$/-%03d.png/'`
 
 echo busy ...
 echo wait for this:
 echo $pngn
 
 # use ~/anaconda3/envs/gemini2 imagemagick to convert pdfn to png:
-~/anaconda3/envs/gemini2/bin/magick convert -density 300 $pdfn -quality 100 $pngn
+echo ~/anaconda3/envs/gemini2/bin/magick -density 300 $pdfn -scene 0 -quality 100 $pngn
+
+# Use Tesseract to extract text from png file:
+fn=`echo $pdfn | sed 's/.pdf$//'`
+for pngf in ${fn}*png
+do
+    echo ~/anaconda3/envs/gemini2/bin/tesseract $pngf $pngf
+done
 
 exit
