9da39a642bf4a29178aed209d5f29bedbc7f235f
Fred Tealyodeler <fred@fredt.edu>
2025-03-09 22:08:12 -0700

    more

--- public/python_scripts/pdf_magick_tess34fn_gemini.py
+++ public/python_scripts/pdf_magick_tess34fn_gemini.py
@@ -65,5 +65,6 @@ if 25 >= npg_i:
     cmd_s = 'cp /tmp/urpdf/big.pdf /tmp/urpdf/pg1_25.pdf' # just use big.pdf
 else:
     cmd_s = '{qpdf} /tmp/urpdf/big.pdf --pages . 1-25 -- /tmp/urpdf/pg1_25.pdf'
+
 sshell2(cmd_s)
 
